3bf4d0c7b2c6572d0adaa0531991523614070ab5
lohewrergdhfdsawfgt <163724776+lohewrergdhfdsawfgt@users.noreply.github.com>
2024-03-31 11:22:15 +0300

    Update shop_run.py
    аррвпарав

--- shop_run.py
+++ shop_run.py
@@ -1,4 +1,47 @@
 import tkinter as tk
 
+from start_loc_file import start_loc, def_killer
+hp = 10
+gold = 0
+def def_lottery(event):
+    button1.grid_remove()
+    button2.grid_remove()
+    button3.grid_remove()
+    text_field1 = tk.Label(text='Ты купил билет, ты проиграл', width=56, height=15, bg='yellow')
+    text_field1.grid(row=0)
+gold -= 2
+def def_xleb(event):
+    button1.grid_remove()
+    button2.grid_remove()
+    button3.grid_remove()
+    text_field2 = tk.Label(text='Ты купил хлеб, ты покушал', width=56, height=15, bg='yellow')
+    text_field2.grid(row=0)
+hp += 1
+gold -= 1
+
+def def_maclo(event):
+    button1.grid_remove()
+    button2.grid_remove()
+    button3.grid_remove()
+    text_field3 = tk.Label(text='Ты намазал на хлеб масло, ты стал жертресом', width=56, height=15, bg='yellow')
+    text_field3.grid(row=0)
+hp += 3
+gold -= 2
 def def_shop(event):
-    pass
+    def_killer()
+    text_field = tk.Label(text='Вы пришли в магазин, что ты купишь?', width=56, height=15, bg='blue')
+    text_field.grid(row=0)
+
+    global button1, button2, button3
+    button1 = tk.Button(text='Лотерея', bg='red')
+    button1.grid(column=0, row=1)
+    button1.bind("<Button-1>", def_lottery)
+
+    button2 = tk.Button(text='Хлеб', bg='red')
+    button2.grid(column=0, row=2)
+    button2.bind("<Button-1>", def_xleb)
+
+    button3 = tk.Button(text='Масло', bg='red')
+    button3.grid(column=0, row=3)
+    button3.bind("<Button-1>", def_maclo)
+
